744952f1d0e090e13718d0884f015994b80dda61
developer-boy-sdowner <75466952+developer-boy-sdowner@users.noreply.github.com>
2021-06-16 09:08:29 +0530

    Update aichat.py

--- SheebaQueen/helper_extra/aichat.py
+++ SheebaQueen/helper_extra/aichat.py
@@ -1,30 +1,37 @@
 from SheebaQueen.mongo import client as db_x
 
-sheeba = db_x["CHATBOT"]
-talkmode = db_x["TALKMODE"]
+lydia = db_x["CAHTBOT"]
 
 
 def add_chat(chat_id):
-    stark = sheeba.find_one({"chat_id": chat_id})
+    stark = lydia.find_one({"chat_id": chat_id})
     if stark:
         return False
     else:
-        sheeba.insert_one({"chat_id": chat_id})
+        lydia.insert_one({"chat_id": chat_id})
         return True
 
 
 def remove_chat(chat_id):
-    stark = sheeba.find_one({"chat_id": chat_id})
+    stark = lydia.find_one({"chat_id": chat_id})
     if not stark:
         return False
     else:
-        sheeba.delete_one({"chat_id": chat_id})
+        lydia.delete_one({"chat_id": chat_id})
         return True
 
 
+def get_all_chats():
+    r = list(lydia.find())
+    if r:
+        return r
+    else:
+        return False
+
+
 def get_session(chat_id):
-    star = talkmode.find_one({"chat_id": chat_id})
-    if not star:
+    stark = lydia.find_one({"chat_id": chat_id})
+    if not stark:
         return False
     else:
-        return star
+        return stark
