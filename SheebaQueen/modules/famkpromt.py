e0dad2e4fc4fbe9b72692e69eb711b828d1bf4ee
developer-boy-sdowner <75466952+developer-boy-sdowner@users.noreply.github.com>
2021-06-21 14:05:46 +0530

    Update famkpromt.py

--- SheebaQueen/modules/famkpromt.py
+++ SheebaQueen/modules/famkpromt.py
@@ -2,10 +2,11 @@ import asyncio
 
 from SheebaQueen import telethn as sheeba
 from telethon import events
+import re
 
 @sheeba(events.NewMessage(pattern="(/pprank ?(.*)"))
 async def pprank(ult):
-    if not ult.text[0].isalpha() and ult.text[0] not in ("/", "#", "@", "!"):
+   
         msg = await eor(ult, "**PROMOTING USER..**")
         await asyncio.sleep(1)
         await msg.edit("**PROMOTING USER...**")
