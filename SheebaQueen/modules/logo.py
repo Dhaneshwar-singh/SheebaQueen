06d6501320c029a775bd289403161fcb6b8f348f
developer-boy-sdowner <75466952+developer-boy-sdowner@users.noreply.github.com>
2021-07-04 09:28:51 +0530

    Update logo.py

--- SheebaQueen/modules/logo.py
+++ SheebaQueen/modules/logo.py
@@ -7,7 +7,7 @@ import random
 import sys
 import glob
 
-@register(pattern="^/mlogo ?(.*)")
+@register(pattern="^/logo ?(.*)")
 async def lego(event):
  quew = event.pattern_match.group(1)
  if event.sender_id == OWNER_ID:
@@ -39,7 +39,7 @@ async def lego(event):
     draw.text(((image_widthz-w)/2, (image_heightz-h)/2), text, font=font, fill=(255, 255, 255))
     x = (image_widthz-w)/2
     y= ((image_heightz-h)/2+6)
-    draw.text((x, y), text, font=font, fill="black", stroke_width=25, stroke_fill="yellow")
+    draw.text((x, y), text, font=font, fill="white")
     fname2 = "LogoByYone.png"
     img.save(fname2, "png")
     await tbot.send_file(event.chat_id, fname2, caption="Made By SheebaQueen")
