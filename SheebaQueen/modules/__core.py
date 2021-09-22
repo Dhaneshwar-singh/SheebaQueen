cddaafb2ae870e0529fbb659fb9e92fe2bc0dafe
Hunting Bots <69734199+HuntingBots@users.noreply.github.com>
2021-09-22 16:06:22 +0530

    Update __core.py

--- SheebaQueen/modules/__core.py
+++ SheebaQueen/modules/__core.py
@@ -36,3 +36,31 @@ async def Prof(event):
         await event.reply("No File Found!")
 
 
+# recoded by @The_Ghost_Hunter | copyright on SheebaQueen
+
+
+from pyrogram import filters
+
+from SheebaQueen import OWNER_ID
+from SheebaQueen import pbot as app
+
+
+@app.on_message(filters.command("install") & filters.user(OWNER_ID))
+async def install_module(_, message):
+    if not message.reply_to_message:
+        await message.reply_text("Reply To A .py File To Install It.")
+        return
+    if not message.reply_to_message.document:
+        await message.reply_text("Reply To A .py File To Install It.")
+        return
+    document = message.reply_to_message.document
+    if document.mime_type != "text/x-python":
+        await message.reply_text("INVALID_MIME_TYPE, Reply To A Correct .py File.")
+        return
+    m = await message.reply_text("**Installing Module**")
+    await message.reply_to_message.download(f"./SheebaQueen/modules/{document.file_name}")
+    await m.edit("**Restarting**")
+    os.execvp(
+        f"python{str(pyver.split(' ')[0])[:3]}",
+        [f"python{str(pyver.split(' ')[0])[:3]}", "-m", "SheebaQueen"],
+    )
