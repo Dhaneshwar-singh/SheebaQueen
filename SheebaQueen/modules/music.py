837d8ce666e8a538abf2476a106294169686f76d
Hunting Bots <69734199+HuntingBots@users.noreply.github.com>
2021-09-25 18:57:29 +0530

    Update music.py

--- SheebaQueen/modules/music.py
+++ SheebaQueen/modules/music.py
@@ -243,6 +243,34 @@ async def _(client, message):
         await lel.edit(reply)  # edit or reply
 
 
+@pbot.on_message(filters.command("saavn"))
+async def song(client, message):
+    message.chat.id
+    message.from_user["id"]
+    args = get_arg(message) + " " + "song"
+    if args.startswith(" "):
+        await message.reply("<b>Enter song name❗</b>")
+        return ""
+    m = await message.reply_text(
+        "Downloading your song,\nPlz wait ⏳️"
+    )
+    try:
+        r = requests.get(f"https://jostapi.herokuapp.com/saavn?query={args}")
+    except Exception as e:
+        await m.edit(str(e))
+        return
+    sname = r.json()[0]["song"]
+    slink = r.json()[0]["media_url"]
+    ssingers = r.json()[0]["singers"]
+    file = wget.download(slink)
+    ffile = file.replace("mp4", "m4a")
+    os.rename(file, ffile)
+    await message.reply_audio(audio=ffile, title=sname, performer=ssingers)
+    os.remove(ffile)
+    await m.delete()
+
+
+
 @pbot.on_message(filters.command(["glyric", "glyrics"]))
 async def lyrics(client, message):
 
@@ -298,30 +326,3 @@ async def lyrics(client, message):
             f"**Search query**: \n`{artist} - {song}`\n\n```{songs.lyrics}```"
         )
     return
-
-
-@pbot.on_message(filters.command("saavn"))
-async def song(client, message):
-    message.chat.id
-    message.from_user["id"]
-    args = get_arg(message) + " " + "song"
-    if args.startswith(" "):
-        await message.reply("<b>Enter song name❗</b>")
-        return ""
-    m = await message.reply_text(
-        "Downloading your song,\nPlz wait ⏳️"
-    )
-    try:
-        r = requests.get(f"https://jostapi.herokuapp.com/saavn?query={args}")
-    except Exception as e:
-        await m.edit(str(e))
-        return
-    sname = r.json()[0]["song"]
-    slink = r.json()[0]["media_url"]
-    ssingers = r.json()[0]["singers"]
-    file = wget.download(slink)
-    ffile = file.replace("mp4", "m4a")
-    os.rename(file, ffile)
-    await message.reply_audio(audio=ffile, title=sname, performer=ssingers)
-    os.remove(ffile)
-    await m.delete()
