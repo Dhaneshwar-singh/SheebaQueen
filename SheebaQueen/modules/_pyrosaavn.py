a5923037aac739c35c13f848664286776d4da843
Hunting Bots <69734199+HuntingBots@users.noreply.github.com>
2021-09-25 18:44:52 +0530

    Update _pyrosaavn.py

--- SheebaQueen/modules/_pyrosaavn.py
+++ SheebaQueen/modules/_pyrosaavn.py
@@ -1,47 +1,38 @@
-# recoded by @The_Ghost_Hunter on Telegram. 
+# # # Thanks to @infinity_bots - MizukiBot
+import os
+
+#Saavn 
 
 import requests
 import wget
 from pyrogram import filters
 
-from SheebaQueen import pbot as app 
-from SheebaQueen.pyrogramee.dark import get_arg
+from GabiBraunRobot import pbot as app 
+from GabiBraunRobot.pyrogramee.dark import get_arg
 
 
-@app.on_message(filters.command("saavn") & ~filters.edited)
-async def jssong(_, message):
-    global is_downloading
-    if len(message.command) < 2:
-        return await message.reply_text(
-            "/saavn requires an argument."
-        )
-    if is_downloading:
-        return await message.reply_text(
-            "Another download is in progress, try again after sometime."
-        )
-    is_downloading = True
-    text = message.text.split(None, 1)[1]
-    m = await message.reply_text("Searching...")
+@app.on_message(filters.command("saavn"))
+async def song(client, message):
+    message.chat.id
+    message.from_user["id"]
+    args = get_arg(message) + " " + "song"
+    if args.startswith(" "):
+        await message.reply("<b>Enter song name❗</b>")
+        return ""
+    m = await message.reply_text(
+        "Downloading your song,\nPlz wait ⏳️"
+    )
     try:
-        songs = await arq.saavn(text)
-        if not songs.ok:
-            await m.edit(songs.result)
-            is_downloading = False
-            return
-        sname = songs.result[0].song
-        slink = songs.result[0].media_url
-        ssingers = songs.result[0].singers
-        await m.edit("Downloading")
-        song = await download_song(slink)
-        await m.edit("Uploading")
-        await message.reply_audio(
-            audio=song,
-            title=sname,
-            performer=ssingers,
-        )
-        os.remove(song)
-        await m.delete()
+        r = requests.get(f"https://jostapi.herokuapp.com/saavn?query={args}")
     except Exception as e:
-        is_downloading = False
-        return await m.edit(str(e))
-    is_downloading = False
+        await m.edit(str(e))
+        return
+    sname = r.json()[0]["song"]
+    slink = r.json()[0]["media_url"]
+    ssingers = r.json()[0]["singers"]
+    file = wget.download(slink)
+    ffile = file.replace("mp4", "m4a")
+    os.rename(file, ffile)
+    await message.reply_audio(audio=ffile, title=sname, performer=ssingers)
+    os.remove(ffile)
+    await m.delete()
