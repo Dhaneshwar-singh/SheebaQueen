e3e4ffea01de52f3f594cae8207671b3400dbfde
developer-boy-sdowner <75466952+developer-boy-sdowner@users.noreply.github.com>
2021-11-12 15:04:06 +0530

    Create __whisper.py

---
+++ SheebaQueen/modules/__whisper.py
@@ -0,0 +1,9 @@
+__mod_name__ = "Whisper"
+__help__ ="""
+ TO USE WHISPER MODULE JUST FOLOWER BELOW FORMAT ,its an inline moduleso u have to add @Sheeba_Queenbot in first
+ ----------format--------
+ @Sheeba_Queenbot [your message] username or id. of the user ,who you want to send whisper message
+ 
+ EXAMPLE: @Sheeba_Queenbot hi sar i am developer @Developer_boy_sd_owner
+ 
+"""
