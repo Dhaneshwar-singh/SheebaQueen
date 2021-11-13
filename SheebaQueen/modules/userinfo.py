bcab86379a7e3bbf66ca39e41530a479bb482a3a
developer-boy-sdowner <75466952+developer-boy-sdowner@users.noreply.github.com>
2021-11-13 12:44:44 +0530

    Update userinfo.py

--- SheebaQueen/modules/userinfo.py
+++ SheebaQueen/modules/userinfo.py
@@ -306,7 +306,7 @@ def info(update: Update, context: CallbackContext):
         disaster_level_present = True
 
     if disaster_level_present:
-        text += ' [<a href="https://t.me/OnePunchUpdates/155">?</a>]'.format(
+        text += ' [<a href="https://t.me/">?</a>]'.format(
             bot.username
         )
 
@@ -544,7 +544,7 @@ When marked as AFK, any mentions will be replied to with a message to say you're
  ❍ /info*:* get information about a user. 
  
 *What is that health thingy?*
- Come and see [HP System explained](https://t.me/OnePunchUpdates/192)
+ Come and see [HP System explained](https://t.me/)
 """
 
 SET_BIO_HANDLER = DisableAbleCommandHandler("setbio", set_about_bio)
