23f6c975241c93a67a6d53c3d4d2e6639cd1a92a
developer-boy-sdowner <75466952+developer-boy-sdowner@users.noreply.github.com>
2021-11-18 15:58:48 +0530

    Create __repo.py

---
+++ SheebaQueen/modules/__repo.py
@@ -0,0 +1,14 @@
+from SheebaQueen.events import register
+from telethon import Button
+
+
+@register(pattern="^/repo")
+async def repo(event): 
+  await event.reply(" credit to 
+                    MY MASTER - [Developer-BOY](https://t.me/Developer_boy_sd_owner)[GITHUB](https://github.com/developer-boy-sdowner)
+                    [noob-kittu](https://github.com/noob-kittu)
+                    [Ghost-Hunter](https://github.com/HuntingBots)
+                    @Sheeba_Queenbot [GitHub-repository](https://github.com/developer-boy-sdowner/SheebaQueen)
+                    
+                    
+                    "
