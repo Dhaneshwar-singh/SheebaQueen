b806a70877a03c4dfc4fe01c4f6f5a9d7530768a
developer-boy-sdowner <75466952+developer-boy-sdowner@users.noreply.github.com>
2021-11-18 16:05:33 +0530

    Update __repo.py

--- SheebaQueen/modules/__repo.py
+++ SheebaQueen/modules/__repo.py
@@ -4,11 +4,11 @@ from telethon import Button
 
 @register(pattern="^/repo")
 async def repo(event): 
-  await event.reply(" credit to 
+  await event.reply(""" credit to 
                     MY MASTER - [Developer-BOY](https://t.me/Developer_boy_sd_owner)[GITHUB](https://github.com/developer-boy-sdowner)
                     [noob-kittu](https://github.com/noob-kittu)
                     [Ghost-Hunter](https://github.com/HuntingBots)
                     @Sheeba_Queenbot [GitHub-repository](https://github.com/developer-boy-sdowner/Sheebarobot)
                     
                     
-                    "
+                    """
