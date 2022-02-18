05e5a0130008a9d136e7d912012bc0b4c950f317
Dhaneshwar Singh <75466952+Dhaneshwar-singh@users.noreply.github.com>
2022-02-18 14:45:01 +0530

    Update __repo.py

--- SheebaQueen/modules/__repo.py
+++ SheebaQueen/modules/__repo.py
@@ -5,8 +5,8 @@ from telethon import Button
 @register(pattern="^/repo")
 async def repo(event): 
   await event.reply(""" Credit to
- 👨‍💻  MY MASTER - [Developer-BOY](https://t.me/Developer_boy_d) 🤴 [GITHUB](https://github.com/developer-boy-sdowner)
+ 👨‍💻  MY MASTER - [Developer-BOY](https://t.me/Developer_boy_d) 🤴 [GITHUB](https://github.com/Dhaneshwar-singh)
       ------------------------------------------------------                  
  👉  
       ------------------------------------------------------                  
- 🌝@Sheeba_Queenbot [GitHub-repository](https://github.com/developer-boy-sdowner/Sheebarobot)""")
+ 🌝@Sheeba_Queenbot [GitHub-repository](https://github.com/Dhaneshwar-singh/Sheebarobot)""")
